fe18d40d3a8e2bfc4568dce74b1e3bd29513abf4
Tomas Matousek <tomas.matousekst@gmail.com>
2026-03-15 11:16:56 +0100

    change size button and mono hover effect

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('#grid-container');
+const changeSize = document.querySelector('#knobLeft');
+
+knobLeft.addEventListener('click', () => {
+    createGrid(prompt('Choose number from 2 to 80'));
+});
+
 
 function createGrid(quantity) {
     container.innerHTML = '';
-    const squareSize = 800 / quantity;
+    const squareSize = 562 / quantity;
 
     for (let i = 0; i < quantity * quantity; i++) {
         const square = document.createElement('div');
         square.style.width =  squareSize + 'px';
         square.style.height = squareSize + 'px';
-        square.style.outline = '2px solid black';
+        square.style.outline = '0.2px solid grey';
         container.appendChild(square);
-    }
-}
 
-createGrid(100);
+        square.addEventListener ('mousemove', () => {
+            square.style.backgroundColor = 'black';
+        });
+    };
+};
 
-//make divs grow into parent
-//make divs into small squares
+
+//grid hover effect
+//grid color reset
+//rgb button

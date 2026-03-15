50dc2d0417773f035bc977ca8472304fb202dc30
Tomas Matousek <tomas.matousekst@gmail.com>
2026-03-15 12:02:14 +0100

    Reset button, RBG button, final changes

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const container = document.querySelector('#grid-container');
 const changeSize = document.querySelector('#knobLeft');
+const resetGrid = document.querySelector('#shakeBtn');
+const rgbButton = document.querySelector('#knobRight');
+let rgbIsClicked = false;
 
-knobLeft.addEventListener('click', () => {
-    createGrid(prompt('Choose number from 2 to 80'));
+//Generate grid based on user input
+changeSize.addEventListener('click', () => {
+    //createGrid(prompt('Choose number from 2 to 80'));
+    const userNumber = prompt('Choose number from 2 to 80');
+    if (userNumber > 80 || userNumber < 2) {
+        alert('Incorrect number entered');
+    }   else createGrid(userNumber);
 });
 
+//reset grid to blank
+resetGrid.addEventListener('click', () => {
+    container.innerHTML = '';
+});
 
+//toggle button for RGB coloring
+rgbButton.addEventListener('click', () => {
+    rgbIsClicked = !rgbIsClicked;
+})
+
+//loops until user input number reached
+//mouse coloring function
 function createGrid(quantity) {
     container.innerHTML = '';
     const squareSize = 562 / quantity;
@@ -17,13 +36,16 @@ function createGrid(quantity) {
         square.style.outline = '0.2px solid grey';
         container.appendChild(square);
 
-        square.addEventListener ('mousemove', () => {
-            square.style.backgroundColor = 'black';
+        square.addEventListener ('mouseleave', () => {
+            if (rgbIsClicked == false) {
+                square.style.backgroundColor = 'black';
+            }   else if (rgbIsClicked == true) {
+                const red = Math.round(Math.random() * 255);
+                const green = Math.round(Math.random() * 255);
+                const blue = Math.round(Math.random() * 255);
+                square.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+            }
         });
     };
+    console.log(rgbIsClicked);
 };
-
-
-//grid hover effect
-//grid color reset
-//rgb button
